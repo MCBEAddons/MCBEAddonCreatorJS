15f56d2f5396790e8db2b3cf3c15a9a59d39f748
Noinkin <87577662+Noinkin@users.noreply.github.com>
2022-03-28 15:32:35 +0000

    New package, UUID, compiler in progress

--- mcbeaddoncreatorjs/src/compiler/compile.js
+++ mcbeaddoncreatorjs/src/compiler/compile.js
@@ -1,3 +1,7 @@
+import { v4 as uuidv4 } from 'uuid'
+
 function compile(builders) {
     fs.mkdirSync('./builds', { recursive: true })
+	fs.mkdirSync('./builds/rp', { recursive: true })
+	fs.mkdirSync('./builds/bp', { recursive: true })
 }
