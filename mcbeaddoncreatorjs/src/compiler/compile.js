a1a9788c69f9dd96c12263092cc4246305e943be
Noinkin <87577662+Noinkin@users.noreply.github.com>
2022-03-28 19:24:04 +0000

    UUID Gen and Compiler V0.1.0

--- mcbeaddoncreatorjs/src/compiler/compile.js
+++ mcbeaddoncreatorjs/src/compiler/compile.js
@@ -1,7 +1,129 @@
-import { v4 as uuidv4 } from 'uuid'
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs')
+let standardRPManifest = {
+  "format_version": 2,
+  "header": {
+    "name": "MCBE Addon Creator JS Addon RP",
+    "description": "This addon was made using MCBE Addon Creator JS",
+    "uuid": uuidv4(),
+    "version": [
+      1,
+      0,
+      0
+    ],
+    "min_engine_version": [
+      1,
+      17,
+      0
+    ]
+  },
+  "modules": [
+    {
+      "type": "resources",
+      "uuid": uuidv4(),
+      "version": [
+        1,
+        0,
+        0
+      ]
+    }
+  ],
+  "metadata": {
+    "authors": [
+      "Noinkin#5026"
+    ],
+    "url": "MCBEAddons.tk"
+  }
+}
 
-function compile(builders) {
-    fs.mkdirSync('./builds', { recursive: true })
-	fs.mkdirSync('./builds/rp', { recursive: true })
-	fs.mkdirSync('./builds/bp', { recursive: true })
+let standardBPManifest = {
+  "format_version": 2,
+  "header": {
+    "name": "MCBE Addon Creator JS Addon",
+    "description": "This addon was made using MCBE Addon Creator JS",
+    "uuid": uuidv4(),
+    "version": [
+      1,
+      0,
+      0
+    ],
+    "min_engine_version": [
+      1,
+      17,
+      0
+    ]
+  },
+  "modules": [
+    {
+      "type": "data",
+      "uuid": uuidv4(),
+      "version": [
+        1,
+        0,
+        0
+      ]
+    }
+  ],
+  "metadata": {
+    "authors": [
+      "Noinkin#5026"
+    ],
+    "url": "MCBEAddons.tk"
+  }
+}
+
+class Compiler {
+
+    #manifestRP = standardRPManifest
+    #manifestBP = standardBPManifest
+    #components = null
+	
+    static Compile = class {
+		
+        #manifestRP = standardRPManifest
+        #manifestBP = standardBPManifest
+        #components = null
+		
+        setRPManifest(manifestRP) {
+            this.#manifestRP = manifestRP
+            return this
+        }
+
+        setBPManifest(manifestBP) {
+            this.#manifestBP = manifestBP
+            return this
+        }
+
+        setComponents(components) {
+            this.#components = components
+            return this
+        }
+		
+        compile() {
+            const compiled = new Compiler(
+                this.#manifestRP,
+                this.#manifestBP,
+                this.#components)
+            return compiled
+        }
+    }
+
+    constructor(manifestRP, manifestBP, components) {
+        this.#manifestRP = manifestRP
+        this.#manifestBP = manifestBP
+        this.#components = components
+    }
+
+    compile() {
+		console.log('Compiling')
+		let data1 = JSON.stringify(this.#manifestRP)
+		let data2 = JSON.stringify(this.#manifestBP)
+    	fs.mkdirSync('./builds', { recursive: true })
+		fs.mkdirSync('./builds/rp', { recursive: true })
+		fs.mkdirSync('./builds/bp', { recursive: true })
+		fs.writeFile('./builds/rp/manifest.json', data1, (err) => {console.log(err)});
+		fs.writeFile('./builds/bp/manifest.json', data2, (err) => {console.log(err)});
+		console.log('Compiled')
+    }
+
 }
