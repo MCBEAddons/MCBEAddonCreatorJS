12247c80920b7fab36c7757b913e13786838a86a
Noinkin <87577662+Noinkin@users.noreply.github.com>
2022-03-30 20:35:24 +0000

    Updated index.js Imported BPManifest

--- mcbeaddoncreatorjs/src/index.js
+++ mcbeaddoncreatorjs/src/index.js
@@ -7,3 +7,5 @@ exports.Block = require('./block/block')
 /* Compile Classes */
 exports.Compiler = require('./compiler/compiler')
 
+/* Manifest Classes */
+exports.BPManifest = require('./manifest/bpmanifest')
